e48966a62deaba952a303be486c67e0ff5f026e6
Dmitry Gorshkov <dgorshkov2015@mail.ru>
2022-10-22 17:59:00 +0300

    home-work-4

---
+++ home-work-4/index.js
@@ -0,0 +1,54 @@
+import {readdir } from 'node:fs/promises'
+
+const concatPath = (currentPath, item) => {
+	return currentPath[currentPath.length - 1] === '/' ? currentPath + item.name : `${currentPath}/${item.name}`
+}
+
+const tree = async (path) => {
+	const fileStructure = {
+		files: [],
+		'dirs': [path],
+	}
+	const queue = [path]
+
+	while (queue.length) {
+		const currentPath  = queue.shift()
+		const dirents = await readdir(currentPath, {withFileTypes: true})
+		const files = dirents.filter((item) => item.isFile())
+		const dirs = dirents.filter((item) => item.isDirectory())
+
+		files.forEach((item) => {
+			fileStructure.files.push(concatPath(currentPath, item))
+		})
+
+		dirs.forEach((item) => {
+			const newPath = concatPath(currentPath, item)
+
+			fileStructure.dirs.push(newPath)
+			queue.push(newPath)
+		})
+
+	}
+		// const getStructureByPath = async () => {
+
+
+	// await getStructureByPath()
+	console.log(fileStructure)
+
+}
+
+tree('foo/')
+
+// {
+// 	"files": [
+// 		"foo/f1.txt",
+// 		"foo/f2.txt",
+// 		"foo/bar/bar1.txt",
+// 		"foo/bar/bar2.txt"
+// 	],
+// 	"dirs": [
+// 		"foo",
+// 		"foo/bar",
+// 		"foo/bar/baz"
+// 	]
+// }
