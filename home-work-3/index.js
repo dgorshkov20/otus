93ca94af852d72181900fd7b11602462e8457c58
Dmitry Gorshkov <dgorshkov2015@mail.ru>
2022-11-07 20:49:01 +0300

    home-work/3

--- home-work-3/index.js
+++ home-work-3/index.js
@@ -4,11 +4,31 @@ const getPath = (element) => {
 
 	while (currentElem) {
 		parents.push(currentElem)
-		currentElem = currentElem.parentNode
+		currentElem = currentElem.parentElement
 	}
 
-	return parents.reverse()
-}
+	const attributeToSelector = {
+		'class': '.',
+		'id': '#'
+	}
+	const attributes = []
 
-getPath($0)
+	parents.forEach((elem) => {
+			const tagName = elem.tagName.toLowerCase()
+			if (!elem.getAttributeNames().length) {
+				attributes.push(tagName)
+			} else {
+				for (const name of elem.getAttributeNames()) {
+					const value = elem.getAttribute(name);
+					if (attributeToSelector[name]) {
+						attributes.push(`${tagName}${attributeToSelector[name]}${value}`)
+					}
+				}
+			}
+
+	})
+
+	return attributes.reverse().join(' ')
+}
+console.log(getPath(document.querySelector('li')))
 
